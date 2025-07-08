c9bd220ae8701b58d5127af369d4f2bc35134e07
onegood07 <one_good07@naver.com>
2025-07-09 05:44:26 +0900

    feat: ChattingPage, LoginPage 경로 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./styles/theme";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import ChattingPage from "./pages/ChattingPage";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <LoginPage />
+      <Router>
+        <Routes>
+          <Route path="/" element={<ChattingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
