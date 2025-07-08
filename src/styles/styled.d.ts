6b2b7f9594f6c4bfd1c60e940219beafef1e202e
onegood07 <one_good07@naver.com>
2025-07-08 22:13:43 +0900

    chore: theme 적용을 위한 타입 확장

---
+++ src/styles/styled.d.ts
@@ -0,0 +1,15 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      primary: string;
+      chattingPageBg: string;
+      loginPageBg: string;
+      captionText: string;
+      iconGray: string;
+      strokeGray: string;
+      white: string;
+    };
+  }
+}
