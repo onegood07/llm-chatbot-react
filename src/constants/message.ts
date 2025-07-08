ec085fba0349efefeabd6460f98b329fa71f04d2
onegood07 <one_good07@naver.com>
2025-07-09 05:45:02 +0900

    feat: ChattingPage용 상수 추가

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,4 +1,13 @@
 export const LOGIN_WELCOME_MESSAGE = "어서오세요";
 export const LOGIN_HELPER_TEXT =
   "세종대학교 학사정보시스템과 동일하게 입력하세요!";
+export const CHATTING_HELPER_TEXT =
+  "세종대 수강편람에 기반해 궁금한 걸 알려드릴게요!";
+
 export const FORGOT_PASSWORD_TEXT = "비밀번호를 잊으셨나요?";
+
+export const SAMPLE_SUGGESTED_TEXT = {
+  book: "22학번 대상으로 한 수강편람 요약해줘!",
+  graduationCap: "이번에 졸업하는 소프트웨어학과인데 졸업 조건이 뭐야?",
+  laptop: "소프트웨어학과 신입생인데 1학년 1학기 전필과목 알려줘!",
+};
