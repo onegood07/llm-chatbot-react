f74c08adbc2e33e38d7b4dac06eb17fe95c34c1a
onegood07 <one_good07@naver.com>
2025-07-08 21:57:31 +0900

    chore: theme 색상 팔레트 추가

---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors: {
+    primary: "#267FFF",
+    chattingPageBg: "#FBFBFB",
+    loginPageBg: "#F2F6FF",
+    captionText: "#838383",
+    iconGray: "#828282",
+    strokeGray: "#DADADA",
+  },
+};
