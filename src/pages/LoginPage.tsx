5b51a99bd845f6c55a8ce0efa56589d0306368f3
onegood07 <one_good07@naver.com>
2025-07-08 23:48:08 +0900

    feat: LoginPage 구현

---
+++ src/pages/LoginPage.tsx
@@ -0,0 +1,33 @@
+import * as S from "../pages/Login.styles";
+import {
+  LOGIN_WELCOME_MESSAGE,
+  LOGIN_HELPER_TEXT,
+  FORGOT_PASSWORD_TEXT,
+} from "../constants/message";
+import {
+  LOGIN_BUTTON_TEXT,
+  FIND_PASSWORD_BUTTON_TEXT,
+} from "../constants/buttonText";
+import LoginForm from "../components/LoginForm";
+
+const LoginPage = () => {
+  return (
+    <S.LoginPageContainer>
+      <S.LoginPartContainer>
+        <S.ColumnWrapper>
+          <S.WelcomeMessage>{LOGIN_WELCOME_MESSAGE}</S.WelcomeMessage>
+          <S.HelperText>{LOGIN_HELPER_TEXT}</S.HelperText>
+        </S.ColumnWrapper>
+        <LoginForm inputType="id" />
+        <LoginForm inputType="password" />
+        <S.LoginButton>{LOGIN_BUTTON_TEXT}</S.LoginButton>
+      </S.LoginPartContainer>
+      <S.RowWrapper>
+        <S.HelperText>{FORGOT_PASSWORD_TEXT}</S.HelperText>
+        <S.FindPasswordButton>{FIND_PASSWORD_BUTTON_TEXT}</S.FindPasswordButton>
+      </S.RowWrapper>
+    </S.LoginPageContainer>
+  );
+};
+
+export default LoginPage;
